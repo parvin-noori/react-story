2577a09ba46c2086af89fac43c0c52a93b368134
parvin <parvinnoori0@gmail.com>
2024-06-02 22:18:10 +0330

    insert story and complete array data

--- src/components/story/StoryAvatar.jsx
+++ src/components/story/StoryAvatar.jsx
@@ -6,59 +6,117 @@ import {
   DialogHeader,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import { useState } from "react";
 import Stories from "react-insta-stories";
 
-const stories = [
+const data = [
   {
-    url: "imgs/mario.png",
-    duration: 5000,
-    header: {
-      heading: "Mohit Karekar",
-      subheading: "Posted 30m ago",
+    user: {
+      name: "user 1",
       profileImage: "imgs/luigi.png",
     },
+    stories: [
+      {
+        url: "imgs/mario.png",
+        duration: 5000,
+        header: {
+          heading: "Mohit Karekar",
+          subheading: "Posted 30m ago",
+          profileImage: "imgs/luigi.png",
+        },
+      },
+      {
+        url: "imgs/luigi.png",
+        duration: 5000,
+        header: {
+          heading: "Mohit Karekar",
+          subheading: "Posted 30m ago",
+          profileImage: "imgs/luigi.png",
+        },
+      },
+    ],
   },
   {
-    url: "imgs/luigi.png",
-    duration: 5000,
-    header: {
-      heading: "Mohit Karekar",
-      subheading: "Posted 30m ago",
+    user: {
+      name: "user 2",
       profileImage: "imgs/luigi.png",
     },
+    stories: [
+      {
+        url: "imgs/luigi.png",
+        duration: 5000,
+        header: {
+          heading: "Mohit Karekar",
+          subheading: "Posted 30m ago",
+          profileImage: "imgs/luigi.png",
+        },
+      },
+    ],
   },
 ];
 
 export default function StoryAvatar() {
   const [endSlide, setEndSlide] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [currentStoryIndex, setCurrentStoryIndex] = useState(null);
   const handleSlide = () => {
     setEndSlide(true);
+    setIsDialogOpen(false);
   };
+  const handleStoryStart = (index) => {
+    const currentUserSoty = data[currentStoryIndex].stories;
+    if (index === currentUserSoty - 1) {
+      setEndSlide(true);
+    }
+  };
+
   return (
     <div className={`my-5 ${endSlide ? `end` : ""}`}>
-        <Dialog>
-          <DialogTrigger>
-            <Avatar className="w-20 h-20">
-              <AvatarImage src="imgs/luigi.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogDescription>
-                <Stories
-                  defaultInterval={1500}
-                  width={432}
-                  height={368}
-                  stories={stories}
-                  onAllStoriesEnd={handleSlide}
-                />
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {data.map((userStories, userIndex) => (
+          <SwiperSlide>
+            <Dialog
+              key={userIndex}
+              open={isDialogOpen && currentStoryIndex === userIndex}
+              onOpenChange={(open) => {
+                if (open) {
+                  setCurrentStoryIndex(userIndex);
+                  setIsDialogOpen(true);
+                } else {
+                  setIsDialogOpen(false);
+                }
+              }}
+            >
+              <DialogTrigger>
+                <Avatar className="w-20 h-20">
+                  <AvatarImage src={userStories.user.profileImage} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogDescription>
+                    <Stories
+                      defaultInterval={1500}
+                      width={432}
+                      height={368}
+                      stories={userStories.stories}
+                      onAllStoriesEnd={handleSlide}
+                      onStoryStart={handleStoryStart}
+                    />
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
