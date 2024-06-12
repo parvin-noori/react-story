12cd89819ae47a74a0e06c212d0b16935d3de103
parvin <parvinnoori0@gmail.com>
2024-06-12 07:53:43 +0330

    style of dialog

--- src/components/story/Dialog.jsx
+++ src/components/story/Dialog.jsx
@@ -26,20 +26,18 @@ export default function Modal({
           }
         }}
       >
-          <DialogContent>
-        <div className="w-full h-full">
-            {/* <DialogHeader>
+        <DialogContent className="max-w-full md:max-w-md max-h-full md:max-h-md top-0 md:top-1/2 translate-x-0 md:-translate-x-2/4 left-0 md:left-1/2 translate-y-0  md:-translate-y-2/4 bottom-0">
+          {/* <DialogHeader>
               <DialogDescription> */}
-                <NestedDialogSwiper
-                  data={data}
-                  setIsDialogOpen={setIsDialogOpen}
-                  selectedIndex={selectedIndex}
-                  setEndSlide={setEndSlide}
-                />
-              {/* </DialogDescription>
+          <NestedDialogSwiper
+            data={data}
+            setIsDialogOpen={setIsDialogOpen}
+            selectedIndex={selectedIndex}
+            setEndSlide={setEndSlide}
+          />
+          {/* </DialogDescription>
             </DialogHeader> */}
-        </div>
-          </DialogContent>
+        </DialogContent>
       </Dialog>
     </>
   );
